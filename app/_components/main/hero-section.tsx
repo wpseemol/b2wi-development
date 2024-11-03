83065c25e5241b40933cfbc30db14faaf25bcf90
wpseemol <wpseemol@gmail.com>
2024-11-04 00:14:37 +0600

    branding seciotn creadt and hero section mobile view ready

---
+++ app/_components/main/hero-section.tsx
@@ -0,0 +1,38 @@
+import ScheduleCall from './schedule-call';
+import TickIcon from './tik-icon';
+
+export default function HeroSection() {
+    return (
+        <section
+            className="bg-[image:var(--image-url)] bg-right-top bg-no-repeat bg-cover bg-[#8a3f3fb2] -mt-1 "
+            style={{ '--image-url': `url(${'/images/Home-Banner.png'})` }}>
+            <div className="w-full min-h-[750px] bg-neutral-900/45">
+                <div className="container mx-auto md:px-2 px-3 text-white flex flex-col justify-center gap-3 min-h-[750px]">
+                    <h3 className="text-2xl font-semibold">
+                        Ignite Your Potential with
+                    </h3>
+                    <h2 className="text-6xl font-semibold">Empower Minds</h2>
+                    <p className="text-base font-normal">
+                        Skilling, reskilling, upskilling, and cross-skilling in
+                        different areas.
+                        <br /> Your partner in personal and professional
+                        success.
+                    </p>
+
+                    <div>
+                        <ScheduleCall />
+                        <div className="flex items-center gap-1">
+                            <span className="w-4 h-4 ">
+                                <TickIcon />
+                            </span>
+                            <p className='className="text-base font-normal"'>
+                                First Pearson-Certified Career Counsellor and
+                                Life Coach in Bangladesh
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,84 +1,9 @@
+import HeroSection from './_components/main/hero-section';
+
 export default function HomePage() {
     return (
-        <main className="container mx-auto md:px-3 px-2">
-            <p>this is my testing projects</p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Ratione, saepe voluptates ipsum voluptatibus soluta doloremque
-                magni. Assumenda, magni? Obcaecati veniam culpa quis minus earum
-                rerum unde odit, magni ratione sint.
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque
-                exercitationem aliquid aspernatur nostrum inventore voluptate
-                cupiditate fugiat recusandae illum ea tempore eius, mollitia
-                aperiam ut similique animi illo incidunt minima.
-            </p>
-            <p>this is my testing projects</p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Ratione, saepe voluptates ipsum voluptatibus soluta doloremque
-                magni. Assumenda, magni? Obcaecati veniam culpa quis minus earum
-                rerum unde odit, magni ratione sint.
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque
-                exercitationem aliquid aspernatur nostrum inventore voluptate
-                cupiditate fugiat recusandae illum ea tempore eius, mollitia
-                aperiam ut similique animi illo incidunt minima.
-            </p>
-            <p>this is my testing projects</p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Ratione, saepe voluptates ipsum voluptatibus soluta doloremque
-                magni. Assumenda, magni? Obcaecati veniam culpa quis minus earum
-                rerum unde odit, magni ratione sint.
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque
-                exercitationem aliquid aspernatur nostrum inventore voluptate
-                cupiditate fugiat recusandae illum ea tempore eius, mollitia
-                aperiam ut similique animi illo incidunt minima.
-            </p>
-            <p>this is my testing projects</p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Ratione, saepe voluptates ipsum voluptatibus soluta doloremque
-                magni. Assumenda, magni? Obcaecati veniam culpa quis minus earum
-                rerum unde odit, magni ratione sint.
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque
-                exercitationem aliquid aspernatur nostrum inventore voluptate
-                cupiditate fugiat recusandae illum ea tempore eius, mollitia
-                aperiam ut similique animi illo incidunt minima.
-            </p>
-            <p>this is my testing projects</p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Ratione, saepe voluptates ipsum voluptatibus soluta doloremque
-                magni. Assumenda, magni? Obcaecati veniam culpa quis minus earum
-                rerum unde odit, magni ratione sint.
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque
-                exercitationem aliquid aspernatur nostrum inventore voluptate
-                cupiditate fugiat recusandae illum ea tempore eius, mollitia
-                aperiam ut similique animi illo incidunt minima.
-            </p>
-            <p>this is my testing projects</p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Ratione, saepe voluptates ipsum voluptatibus soluta doloremque
-                magni. Assumenda, magni? Obcaecati veniam culpa quis minus earum
-                rerum unde odit, magni ratione sint.
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque
-                exercitationem aliquid aspernatur nostrum inventore voluptate
-                cupiditate fugiat recusandae illum ea tempore eius, mollitia
-                aperiam ut similique animi illo incidunt minima.
-            </p>
+        <main>
+            <HeroSection />
         </main>
     );
 }
