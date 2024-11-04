7053f9b1d51970e27a2447f1924894b610bb61e8
wpseemol <wpseemol@gmail.com>
2024-11-04 10:15:27 +0600

    branding section image set

--- app/_components/main/branding-section.tsx
+++ app/_components/main/branding-section.tsx
@@ -1,8 +1,27 @@
+import BardSvg from './bard-svg';
+import PointerIcon from './pointer-icon';
+
 export default function BrandingSection() {
     return (
         <section className="flex justify-center">
-            <div className="border-r min-h-[140px]">left site</div>
-            <div className="border-l min-h-[140px]">right site</div>
+            <div className=" min-h-[140px] bg-primaryColor dark:bg-[#023153] w-1/2 flex items-center justify-end pr-5 relative">
+                <div className="flex justify-end gap-1">
+                    <span className="w-5 h-5 mt-1">
+                        <PointerIcon />
+                    </span>
+                    <p className="text-[18px] font-semibold text-white">
+                        In Collaboration with top-tiered training professionals
+                        throughout <br /> global training industry
+                    </p>
+                    <span className=" w-16 absolute bottom-2 right-5 ">
+                        <BardSvg />
+                        <span className="absolute top-0 left-0  w-full h-full bg-primaryColor/55"></span>
+                    </span>
+                </div>
+            </div>
+            <div className=" min-h-[140px] w-1/2 bg-[#5d5d5d] dark:bg-transparent">
+                right site
+            </div>
         </section>
     );
 }
