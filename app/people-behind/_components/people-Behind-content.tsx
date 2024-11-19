5f5b38e499d56281ea2c36279eab2e985d3777ac
wpseemol <wpseemol@gmail.com>
2024-11-19 23:29:59 +0600

    people behind section keep dada here

---
+++ app/people-behind/_components/people-Behind-content.tsx
@@ -0,0 +1,3 @@
+export default function PeopleBehindContent() {
+    return <div>this is some section here</div>;
+}
--- app/people-behind/page.tsx
+++ app/people-behind/page.tsx
@@ -1,11 +1,13 @@
 import BreadcrumbFirstLink from '@/components/breadcrumb-first-link';
 import HeaderContent from './_components/header-content';
+import PeopleBehindContent from './_components/people-Behind-content';
 
 export default function PeopleBehindPage() {
     return (
         <main className="container mx-auto">
             <BreadcrumbFirstLink currentPageName="People Behind" />
             <HeaderContent />
+            <PeopleBehindContent />
         </main>
     );
 }
