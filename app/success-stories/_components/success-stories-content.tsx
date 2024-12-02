cc0b8b1ed5a1eca02159ee5bf078b913d96786d1
wpseemol <wpseemol@gmail.com>
2024-12-02 15:10:26 +0600

    success stories content map

--- app/success-stories/_components/success-stories-content.tsx
+++ app/success-stories/_components/success-stories-content.tsx
@@ -1,3 +1,13 @@
+import { successStories } from '@/lib/success-stories';
+
 export default function SuccessStoriesContent() {
-    return <div>content</div>;
+    return (
+        <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1">
+            {successStories.map((stories) => (
+                <div key={stories.id}>
+                    <h2>{stories.title}</h2>
+                </div>
+            ))}
+        </div>
+    );
 }
