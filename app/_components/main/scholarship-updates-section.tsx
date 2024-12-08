460a6f18cf9f432d925edafcb59091250f057698
wpseemol <wpseemol@gmail.com>
2024-12-08 13:19:42 +0600

    in home page update data

--- app/_components/main/scholarship-updates-section.tsx
+++ app/_components/main/scholarship-updates-section.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 import ScholarshipUpdatesCarousel from './scholarship-updates-carousel';
 
 export default function ScholarshipUpdates() {
@@ -16,12 +17,14 @@ export default function ScholarshipUpdates() {
                         Scholarship Updates
                     </h2>
                 </div>
-                <Button
-                    size="lg"
-                    variant="outline"
-                    className="border-primaryColor h-12 hover:bg-[#005694] hover:text-neutral-100/80">
-                    Read all Scholarship Updates
-                </Button>
+                <Link href="/scholarships">
+                    <Button
+                        size="lg"
+                        variant="outline"
+                        className="border-primaryColor h-12 hover:bg-[#005694] hover:text-neutral-100/80">
+                        Read all Scholarship Updates
+                    </Button>
+                </Link>
             </div>
             {/* slider section */}
             <ScholarshipUpdatesCarousel />
