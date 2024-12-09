7b16f5d7e7d31c0b410b7278fdf27fea18dce256
wpseemol <wpseemol@gmail.com>
2024-12-09 11:29:55 +0600

    about page some change

--- app/how-do-we-help/page.tsx
+++ app/how-do-we-help/page.tsx
@@ -1,19 +1,23 @@
 import BreadcrumbFirstLink from '@/components/breadcrumb-first-link';
 import HelpCategory from './_components/help-category';
-import HelpSearchSection from './_components/help-search-section';
 import HelpSectionTabView from './_components/help-section-tabview';
 
 export default function HowDoWeHelpPage() {
     return (
         <main className="">
-            <section className="bg-primaryColor md:mb-16 mb-14">
-                <div className="container mx-auto">
-                    <BreadcrumbFirstLink
-                        currentPageName="How Do We Help"
-                        activeClassName="text-white"
-                    />
-                    <HelpSearchSection />
-                </div>
+            <section className="container mx-auto">
+                <BreadcrumbFirstLink currentPageName="About" />
+            </section>
+
+            <section className="text-center py-5 container mx-auto">
+                <h2 className="text-neutral-900/85 sm:text-[52px] text-3xl font-bold leading-snug">
+                    How Does B2WI Help.
+                </h2>
+                <p className="font-normal text-neutral-800/80">
+                    At B2W, we take a holistic approach to ensure you receive
+                    support at every stage of your academic journey.
+                    <br /> This is how we support our aspirants:
+                </p>
             </section>
             <HelpCategory />
             <HelpSectionTabView />
